f3c5c70b856eb30dc24273990c26abf9fbfd03a4
Thierry CANTENOT <thierrycantenot@gmail.com>
2017-09-04 18:55:07 +0200

    Fix warnings

--- code/main.cpp
+++ code/main.cpp
@@ -66,19 +66,27 @@ DISABLE_WARNING_GCC(unused-parameter)
 DISABLE_WARNING_GCC(ignored-qualifiers)
 DISABLE_WARNING_GCC(type-limits)
 DISABLE_WARNING_GCC(empty-body)
+DISABLE_WARNING_GCC(unused-variable)
 
 DISABLE_WARNING_CLANG(sign-conversion)
 DISABLE_WARNING_CLANG(ignored-qualifiers)
 DISABLE_WARNING_CLANG(shadow-field-in-constructor)
 DISABLE_WARNING_CLANG(shadow)
+DISABLE_WARNING_CLANGNG(extra-semi)
+DISABLE_WARNING_CLANG(unused-variable)
+DISABLE_WARNING_CLANG(unused-parameter)
 
 #include <gli/gli.hpp>
 
+ENABLE_WARNING_CLANG(unused-parameter)
+ENABLE_WARNING_CLANG(unused-variable)
+ENABLE_WARNING_CLANG(extra-semi)
 ENABLE_WARNING_CLANG(shadow)
 ENABLE_WARNING_CLANG(shadow-field-in-constructor)
 ENABLE_WARNING_CLANG(ignored-qualifiers)
 ENABLE_WARNING_CLANG(sign-conversion)
 
+ENABLE_WARNING_GCC(unused-variable)
 ENABLE_WARNING_GCC(empty-body)
 ENABLE_WARNING_GCC(type-limits)
 ENABLE_WARNING_GCC(ignored-qualifiers)
